78e4f34ef28f7f5c1821296f42e189b7053793c0
David Arsenault <dmarsenault@gmail.com>
2016-01-27 15:15:49 -0700

    Updated and compelted headings

--- lib/app.rb
+++ lib/app.rb
@@ -17,49 +17,66 @@ def todays_date
 end
 
 def print_divider
-	"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 end
 
 setup_files
 
-#def product_info
-	#$products_hash["items"].each do |toy|
-		#puts "Product Name: #{toy["title"]}"
-		#puts "Retail Price: $#{toy["full-price"]}"
-		#puts "Total Purchases: #{toy["purchases"].length}"
-	#end
-#end
 
-#def header_art(ascii = {})
+
+
+def heading_report
+	puts " "
+	puts "  __                         __                         "
+	puts " /         /                /  |                     /  "
+	puts "(___  ___ (  ___  ___      (___| ___  ___  ___  ___ (___"
+	puts "    )|   )| |___)|___      | |  |___)|   )|   )|   )|   "
+	puts " __/ |__/|| |__   __/      |  | |__  |__/ |__/ |    |__ "
+	puts "                                     |                  "
+	puts " "
+end
+
+def heading_products
+	puts "                     _            _       "
+	puts "                    | |          | |      "
+	puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+	puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+	puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+	puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+	puts "| |                                       "
+	puts "|_|                                       "
+end
+
+
+def heading_brands
+	puts " _                         _     "
+	puts "| |                       | |    "
+	puts "| |__  _ __ __ _ _ __   __| |___ "
+	puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+	puts "| |_) | | | (_| | | | | (_| \\__ \\"
+	puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+	puts
+end
+
+def headings(heading)
+	if heading == :report_heading
+		heading_report
+	elsif heading == :products_heading
+		heading_products
+	elsif heading == :brands_heading
+		heading_brands
+	end
+end
 
 # Print "Sales Report" in ascii art
-puts " "
-puts "  __                         __                         "
-puts " /         /                /  |                     /  "
-puts "(___  ___ (  ___  ___      (___| ___  ___  ___  ___ (___"
-puts "    )|   )| |___)|___      | |  |___)|   )|   )|   )|   "
-puts " __/ |__/|| |__   __/      |  | |__  |__/ |__/ |    |__ "
-puts "                                     |                  "
-puts " "
 
 # Print today's date
-puts print_divider
-puts todays_date
-puts print_divider
 
 # Print "Products" in ascii art
-puts "                     _            _       "
-puts "                    | |          | |      "
-puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
-puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
-puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
-puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
-puts "| |                                       "
-puts "|_|                                       "
 
 # For each product in the data set:
 	# Print the name of the toy
-product_info
+
 	# Print the retail price of the toy
 	# Calculate and print the total number of purchases
 	# Calculate and print the total amount of sales
@@ -67,14 +84,6 @@ product_info
 	# Calculate and print the average discount (% or $) based off the average sales price
 
 # Print "Brands" in ascii art
-puts " _                         _     "
-puts "| |                       | |    "
-puts "| |__  _ __ __ _ _ __   __| |___ "
-puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
-puts "| |_) | | | (_| | | | | (_| \\__ \\"
-puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
-puts
-
 
 # For each brand in the data set:
 	# Print the name of the brand
